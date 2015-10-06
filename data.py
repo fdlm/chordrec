070c43b2406649328d85bf355e44fc6aa2cd974b
Filip Korzeniowski <filip.korzeniowski@jku.at>
2015-10-06 15:35:05 +0200

    moved Dataset class to dmgr

--- data.py
+++ data.py
@@ -1,9 +1,10 @@
 import os
-import madmom as mm
-import numpy as np
 import string
-import dmgr
 
+import numpy as np
+
+import madmom as mm
+import dmgr
 
 FPS = 20
 DATA_DIR = 'data'
@@ -215,111 +216,6 @@ def get_preprocessed_context_datasources(files, preprocessors, context_size,
     )
 
 
-class Dataset:
-    """
-    Class for easier dataset loading
-    """
-
-    def __init__(self, data_dir, feature_cache_dir, split_defs):
-        """
-        Initialises the dataset class
-        :param data_dir:          dataset base directory
-        :param feature_cache_dir: directory where to store cached features
-        :param split_defs         files containing the fold split definitions
-        """
-
-        src_files = dmgr.files.expand(data_dir, '*' + SRC_EXT)
-        gt_files = dmgr.files.expand(data_dir, '*' + GT_EXT)
-        gt_files = dmgr.files.match_files(src_files, gt_files,
-                                          SRC_EXT, GT_EXT)
-
-        feat_files, target_files = dmgr.files.prepare(
-            src_files, gt_files, feature_cache_dir,
-            compute_feat=compute_features,
-            compute_targets=compute_targets,
-            fps=FPS
-        )
-
-        self.feature_files = feat_files
-        self.target_files = target_files
-        self.gt_files = gt_files
-
-        self.split_defs = split_defs
-
-    def get_fold_split(self, val_fold=0, test_fold=1):
-        """
-        Creates a file dictionary (as used by get_preprocessed_datasource),
-        where train, validation, and test folds are pre-defined in split
-        files.
-        :param val_fold:  index of validation fold
-        :param test_fold: index of test fold
-        :return: file dictionary
-        """
-        if not self.split_defs:
-            raise RuntimeError('No cross-validation folds defined!')
-
-        train_feat, val_feat, test_feat = \
-            dmgr.files.predefined_train_val_test_split(
-                self.feature_files,
-                self.split_defs[val_fold],
-                self.split_defs[test_fold],
-                match_suffix=dmgr.files.FEAT_EXT
-            )
-
-        train_targ = dmgr.files.match_files(train_feat, self.target_files,
-                                            dmgr.files.FEAT_EXT,
-                                            dmgr.files.TARGET_EXT)
-        val_targ = dmgr.files.match_files(val_feat, self.target_files,
-                                          dmgr.files.FEAT_EXT,
-                                          dmgr.files.TARGET_EXT)
-        test_targ = dmgr.files.match_files(test_feat, self.target_files,
-                                           dmgr.files.FEAT_EXT,
-                                           dmgr.files.TARGET_EXT)
-
-        return {'train': {'feat': train_feat,
-                          'targ': train_targ},
-                'val': {'feat': val_feat,
-                        'targ': val_targ},
-                'test': {'feat': test_feat,
-                         'targ': test_targ}}
-
-    def get_rand_split(self, val_perc=0.2, test_perc=0.2,
-                       random=np.random.RandomState(seed=0)):
-        """
-        Creates a file dictionary (as used by get_preprocessed_datasource),
-        where train, validation, and test folds are created randomly.
-        :param val_perc:  percentage of files to be used for validation
-        :param test_perc: percentage of files to be used for testing
-        :param random:    random state
-        :return: file dictionary
-        """
-        indices = np.arange(len(self.feature_files))
-        random.shuffle(indices)
-        n_test_files = int(len(indices) * test_perc)
-        n_val_files = int(len(indices) * val_perc)
-
-        test_feat = self.feature_files[:n_test_files]
-        val_feat = self.feature_files[n_test_files:n_test_files + n_val_files]
-        train_feat = self.feature_files[n_test_files + n_val_files:]
-
-        train_targ = dmgr.files.match_files(train_feat, self.target_files,
-                                            dmgr.files.FEAT_EXT,
-                                            dmgr.files.TARGET_EXT)
-        val_targ = dmgr.files.match_files(val_feat, self.target_files,
-                                          dmgr.files.FEAT_EXT,
-                                          dmgr.files.TARGET_EXT)
-        test_targ = dmgr.files.match_files(test_feat, self.target_files,
-                                           dmgr.files.FEAT_EXT,
-                                           dmgr.files.TARGET_EXT)
-
-        return {'train': {'feat': train_feat,
-                          'targ': train_targ},
-                'val': {'feat': val_feat,
-                        'targ': val_targ},
-                'test': {'feat': test_feat,
-                         'targ': test_targ}}
-
-
 def combine_files(*args):
     """
     Combines file dictionaries as returned by the methods of Dataset.
@@ -343,28 +239,43 @@ def combine_files(*args):
 
 
 def load_beatles_dataset(data_dir=DATA_DIR, feature_cache_dir=CACHE_DIR):
-    return Dataset(
+    return dmgr.Dataset(
         os.path.join(data_dir, 'beatles'),
         os.path.join(feature_cache_dir, 'beatles'),
         [os.path.join(data_dir, 'beatles', 'splits',
                       '8-fold_cv_album_distributed_{}.fold'.format(f))
-         for f in range(8)]
+         for f in range(8)],
+        source_ext=SRC_EXT,
+        gt_ext=GT_EXT,
+        compute_features=compute_features,
+        compute_targets=compute_targets,
+        fps=FPS
     )
 
 
 def load_mirex09_dataset(data_dir=DATA_DIR, feature_cache_dir=CACHE_DIR):
-    return Dataset(
+    return dmgr.Dataset(
         os.path.join(data_dir, 'chords_mirex09'),
         os.path.join(feature_cache_dir, 'chords_mirex09'),
-        None
+        None,
+        source_ext=SRC_EXT,
+        gt_ext=GT_EXT,
+        compute_features=compute_features,
+        compute_targets=compute_targets,
+        fps=FPS
     )
 
 
 def load_robbie_dataset(data_dir=DATA_DIR, feature_cache_dir=CACHE_DIR):
-    return Dataset(
+    return dmgr.Dataset(
         os.path.join(data_dir, 'robbie_williams'),
         os.path.join(feature_cache_dir, 'robbie_williams'),
         [os.path.join(data_dir, 'robbie_williams', 'splits',
                       '8-fold_cv_random_{}.fold'.format(f))
-         for f in range(8)]
+         for f in range(8)],
+        source_ext=SRC_EXT,
+        gt_ext=GT_EXT,
+        compute_features=compute_features,
+        compute_targets=compute_targets,
+        fps=FPS
     )
