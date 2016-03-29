b44faad3f44894254c15908b37aa302ef0631174
Filip Korzeniowski <filip.korzeniowski@jku.at>
2016-03-29 13:39:51 +0200

    touches before merge

--- evaluate.py
+++ evaluate.py
@@ -11,24 +11,34 @@ USAGE = """
 evaluate.py
 
 Usage:
-    evaluate.py ANN_DIR PRED_DIR
+    evaluate.py [-i IND_RES_FILE] [-o TOT_RES_FILE] FILES...
 
 Arguments:
-    ANN_DIR  directory containing annotations
-    PRED_DIR  directory containing predictions
+    FILES  annotaion or prediction files
+
+Options:
+    -i IND_RES_FILE  file where to store individual results
+    -o TOT_RES_FILE  file where to store total results
 """
 
 
 def main():
     args = docopt(USAGE)
 
-    pred_files = glob(os.path.join(args['PRED_DIR'], '*.chords.txt'))
-    ann_files = dmgr.files.match_files(
-        pred_files,
-        glob(os.path.join(args['ANN_DIR'], '*.chords')),
-        '.chords.txt',
-        '.chords'
+    ann_files = dmgr.files.match_files_single(
+        args['FILES'], '.chords.txt', '.chords')
+
+    pred_files = dmgr.files.match_files(
+        ann_files,
+        args['FILES'],
+        '.chords',
+        '.chords.txt'
     )
+
+    scores, total_length = test.compute_scores(ann_files, pred_files)
+
+    if args['-i']:
+        header = '# song, length, ' + ', '.join(scores[0][-1].keys())
 
     test.print_scores(test.compute_average_scores(ann_files, pred_files))
 
