5b7e51a0b01f03f796fee23fd4ed8cb59cf155be
Filip Korzeniowski <filip.korzeniowski@jku.at>
2016-03-31 09:36:18 +0200

    refactored simple neural networks (dnn, convnet, rnn) to use the same main function, provided by exp_utils

--- convnet.py
+++ convnet.py
@@ -1,29 +1,7 @@
-from __future__ import print_function
-
-import collections
-import os
-
 import theano.tensor as tt
-import yaml
-from sacred import Experiment
-
-import data
-import dmgr
-import dnn
-import features
 import lasagne as lnn
-import nn
-import targets
-import test
-from exp_utils import PickleAndSymlinkObserver, TempDir, create_optimiser
-from nn.utils import Colors
-
-# Initialise Sacred experiment
-ex = Experiment('Convolutional Neural Network')
-ex.observers.append(PickleAndSymlinkObserver())
-data.add_sacred_config(ex)
-features.add_sacred_config(ex)
-targets.add_sacred_config(ex)
+import dnn
+from exp_utils import setup_experiment, run_experiment
 
 
 def compute_loss(prediction, target):
@@ -120,6 +98,11 @@ def build_net(feature_shape, out_size, net):
         raise RuntimeError('Need to specify output architecture!')
 
     return network, input_var, target_var
+
+
+# Initialise Sacred experiment
+ex = setup_experiment('Convolutional Neural Network')
+run_exp = ex.capture(run_experiment)
 
 
 @ex.config
@@ -225,154 +208,5 @@ def learn_rate_schedule():
 
 
 @ex.automain
-def main(_config, _run, observations, datasource, net, feature_extractor,
-         regularisation, target, optimiser, training, testing):
-
-    if feature_extractor is None:
-        print(Colors.red('ERROR: Specify a feature extractor!'))
-        return 1
-
-    if target is None:
-        print(Colors.red('ERROR: Specify a target!'))
-        return 1
-
-    target_computer = targets.create_target(
-        feature_extractor['params']['fps'],
-        target
-    )
-
-    if not isinstance(datasource['test_fold'], collections.Iterable):
-        datasource['test_fold'] = [datasource['test_fold']]
-
-    if not isinstance(datasource['val_fold'], collections.Iterable):
-        datasource['val_fold'] = [datasource['val_fold']]
-
-        # if no validation folds are specified, always use the
-        # 'None' and determine validation fold automatically
-        if datasource['val_fold'][0] is None:
-            datasource['val_fold'] *= len(datasource['test_fold'])
-
-    if len(datasource['test_fold']) != len(datasource['val_fold']):
-        print(Colors.red('ERROR: Need same number of validation and '
-                         'test folds'))
-        return 1
-
-    all_pred_files = []
-    all_gt_files = []
-
-    print(Colors.magenta('\nStarting experiment ' + ex.observers[0].hash()))
-
-    with TempDir() as exp_dir:
-        for test_fold, val_fold in zip(datasource['test_fold'],
-                                       datasource['val_fold']):
-            print('')
-            print(Colors.yellow(
-                '=' * 20 + ' FOLD {} '.format(test_fold) + '=' * 20))
-            # Load data sets
-            print(Colors.red('\nLoading data...\n'))
-
-            train_set, val_set, test_set, gt_files = data.create_datasources(
-                dataset_names=datasource['datasets'],
-                preprocessors=datasource['preprocessors'],
-                compute_features=features.create_extractor(feature_extractor),
-                compute_targets=target_computer,
-                context_size=datasource['context_size'],
-                test_fold=test_fold,
-                val_fold=val_fold,
-                cached=datasource['cached']
-            )
-
-            if testing['test_on_val']:
-                test_set = val_set
-
-            print(Colors.blue('Train Set:'))
-            print('\t', train_set)
-            print(Colors.blue('Validation Set:'))
-            print('\t', val_set)
-            print(Colors.blue('Test Set:'))
-            print('\t', test_set)
-            print('')
-
-            # build network
-            print(Colors.red('Building network...\n'))
-
-            neural_net, input_var, target_var = build_net(
-                feature_shape=train_set.feature_shape,
-                out_size=train_set.target_shape[0],
-                net=net
-            )
-
-            opt, lrs = create_optimiser(optimiser)
-
-            train_fn = nn.compile_train_fn(
-                neural_net, input_var, target_var,
-                loss_fn=compute_loss, opt_fn=opt,
-                **regularisation
-            )
-
-            test_fn = nn.compile_test_func(
-                neural_net, input_var, target_var,
-                loss_fn=compute_loss,
-                **regularisation
-            )
-
-            process_fn = nn.compile_process_func(neural_net, input_var)
-
-            print(Colors.blue('Neural Network:'))
-            print(nn.to_string(neural_net))
-            print('')
-
-            print(Colors.red('Starting training...\n'))
-
-            train_losses, val_losses, val_accs = nn.train(
-                network=neural_net,
-                train_fn=train_fn, train_set=train_set,
-                test_fn=test_fn, validation_set=val_set,
-                threaded=10, updates=[lrs] if lrs else [],
-                **training
-            )
-
-            print(Colors.red('\nStarting testing...\n'))
-
-            param_file = os.path.join(
-                exp_dir, 'params_fold_{}.pkl'.format(test_fold))
-            nn.save_params(neural_net, param_file)
-            ex.add_artifact(param_file)
-
-            pred_files = test.compute_labeling(
-                process_fn, target_computer, test_set, dest_dir=exp_dir,
-                use_mask=False
-            )
-
-            test_gt_files = dmgr.files.match_files(
-                pred_files, gt_files, test.PREDICTION_EXT, data.GT_EXT
-            )
-
-            all_pred_files += pred_files
-            all_gt_files += test_gt_files
-
-            print(Colors.blue('Results:'))
-            scores = test.compute_average_scores(test_gt_files, pred_files)
-            test.print_scores(scores)
-            result_file = os.path.join(
-                exp_dir, 'results_fold_{}.yaml'.format(test_fold))
-            yaml.dump(dict(scores=scores,
-                           train_losses=map(float, train_losses),
-                           val_losses=map(float, val_losses),
-                           val_accs=map(float, val_accs)),
-                      open(result_file, 'w'))
-            ex.add_artifact(result_file)
-
-        # if there is something to aggregate
-        if len(datasource['test_fold']) > 1:
-            print(Colors.yellow('\nAggregated Results:\n'))
-            scores = test.compute_average_scores(all_gt_files, all_pred_files)
-            test.print_scores(scores)
-            result_file = os.path.join(exp_dir, 'results.yaml')
-            yaml.dump(dict(scores=scores), open(result_file, 'w'))
-            ex.add_artifact(result_file)
-
-        for pf in all_pred_files:
-            ex.add_artifact(pf)
-
-    print(Colors.magenta('Stopping experiment ' + ex.observers[0].hash()))
+def main():
+    run_exp(ex, build_fn=build_net, loss_fn=compute_loss)
